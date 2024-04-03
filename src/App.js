06e8d6a2a9f642bf28046411f4c44ac90f98fa42
Hooveer <mofmasterchauncey@gmail.com>
2024-04-03 22:15:09 +0800

    Initialize Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// Importing browser router, install it first --- npm i -D react-router-dom@latest
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Importing components
+import Template from "./components/Template";
+import Nav from "./components/navbar/Nav";
+import Footer from "./components/footer/Footer";
+import Map from "./components/pages/map/Map";
+import States from "./components/pages/states/States";
+import Effect from "./components/pages/useEffect/Effect";
+import Hooks from "./components/pages/hooks/Hooks";
+import Error from "./components/pages/error/Error";
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <BrowserRouter>
+
+        {/* Importing the Nav component before nested routes */}
+        <Nav />
+
+        {/* Using the Routes and Route components from react-router-dom */}
+        <Routes>
+          <Route path="/" element={<Template />}>
+            <Route path="/states" element={<States />} />
+            <Route path="/effect" element={<Effect />} />
+            <Route path="/map" element={<Map />} />
+            <Route path="/hooks" element={<Hooks />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+
+        {/* Importing the Footer component after nested routes */}
+        <Footer />
+
+      </BrowserRouter>
+      
     </div>
   );
 }
